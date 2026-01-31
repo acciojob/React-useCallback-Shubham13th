d0e9ab02b3a52948acb81cfb6b66c6fc905a70fb
shubh <shubhamsharmajcc@gmail.com>
2026-01-31 20:47:36 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,11 @@ import React from "react";
 
 import "../styles/App.css";
 import { UseCallbackComp } from "./UseCallbackComp";
-// SkillList Component - displays skills and handles deletion
-const SkillList = ({ skills, onDeleteSkill }) => {
-  return (
-    <ul>
-      {skills.map((skill, idx) => (
-        <li key={idx} id={`skill-number-${idx}`} onClick={() => onDeleteSkill(idx)}>
-          {skill}
-        </li>
-      ))}
-    </ul>
-  );
-};
 
-// UseCallbackComp Component - main component for adding and managing skills
-
-// App Component - exports the main component
 const App = () => {
   return <UseCallbackComp />;
 };
 
 export default App;
-export { UseCallbackComp, SkillList };
+
 
